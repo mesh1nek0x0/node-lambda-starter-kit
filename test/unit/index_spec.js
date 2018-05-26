e375c5e2165515eadc716e496b9c1e98abdf504f
mesh1neko <towaduki+gitkraken@gmail.com>
2018-05-26 12:46:11 +0900

    :shirt: fix lint

--- test/unit/index_spec.js
+++ test/unit/index_spec.js
@@ -7,7 +7,11 @@ describe('index_spec test', () => {
     describe('正常系', () => {
         it('sample success', async () => {
             const stub = sinon.stub();
-            await index.handler({result: 'success'}, {context:'foo'}, stub);
+            await index.handler(
+                { result: 'success' },
+                { context: 'foo' },
+                stub
+            );
             expect(stub.callCount).toBe(1);
             expect(stub.args[0][0]).toBeNull();
             expect(stub.args[0][1]).toBe('success end');
@@ -17,7 +21,11 @@ describe('index_spec test', () => {
     describe('異常系', () => {
         it('sample error', async () => {
             const stub = sinon.stub();
-            await index.handler({result: 'fail'}, {context:'foo'}, stub);
+            await index.handler(
+                { result: 'failure' },
+                { context: 'foo' },
+                stub
+            );
             expect(stub.callCount).toBe(1);
             expect(stub.args[0][0]).not.toBeNull();
             expect(stub.args[0][0].message).toBe('error end');
